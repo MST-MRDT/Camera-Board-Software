490119c654158fb61d7a2093d57f1c8a97a42881
andrewvh4 <31864022+andrewvh4@users.noreply.github.com>
2018-05-23 08:34:43 -0500

    Code changes after code review

--- CameraBoard/CameraBoard.ino
+++ CameraBoard/CameraBoard.ino
@@ -14,13 +14,13 @@
 ////////////////////
 /////////////////////////////////////////////////
 //X7 Pins
-const int MUX1_PIN = PK_4;
-const int MUX2_PIN = PK_5;
-const int OE_PIN   = PG_1;
+const int MUX1_A1_PIN           = PK_4;
+const int MUX1_A2_PIN           = PK_5;
+const int OUTPUT_ENABLE1_OE_PIN = PG_1;
 //X9 Pins
-//const int MUX1_PIN = PF_2;
-//const int MUX2_PIN = PF_3;
-//const int OE_PIN   = PF_1; //Output Enable
+//const int MUX2_A1_PIN           = PF_2;
+//const int MUX2_A2_PIN           = PF_3;
+//const int OUTPUT_ENABLE2_OE_PIN = PF_1; //Output Enable
 /*The MUX folows the following truth table (MUX2, MUX1):
  * (0,0): Camera 1
  * (0,1): Camera 2
@@ -37,52 +37,54 @@ RoveCommEthernetUdp RoveComm;
 const uint16_t CAMERA_MUX_CHANNEL = CAMERA_MUX_CHANNEL_1;  //Use this data ID for Camera Board 1 if you have two Camera Boards
 //const uint16_t CAMERA_MUX_CHANNEL = CAMERA_MUX_CHANNEL_2;  //Use this data ID for Camera Board 2 if you have two Camera Boards
 
-//Read Variables/////////
-uint16_t data_id;
-size_t command_data_size;
-uint8_t data_value[2];
-
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 void setup() {
   RoveComm.begin(ROVE_FIRST_OCTET, ROVE_SECOND_OCTET, ROVE_THIRD_OCTET, CAMERABOARD_FOURTH_OCTET);
   delay(10);
   
-  pinMode(MUX1_PIN, OUTPUT);
-  pinMode(MUX2_PIN, OUTPUT);
-  pinMode(OE_PIN,   OUTPUT);
+  pinMode(MUX1_A1_PIN,           OUTPUT);
+  pinMode(MUX1_A2_PIN,           OUTPUT);
+  pinMode(OUTPUT_ENABLE1_OE_PIN, OUTPUT);
 
-  digitalWrite(MUX1_PIN, LOW);
-  digitalWrite(MUX2_PIN, LOW);
-  digitalWrite(OE_PIN,   HIGH);
+  digitalWrite(MUX1_A1_PIN,           LOW);
+  digitalWrite(MUX1_A2_PIN,           LOW);
+  digitalWrite(OUTPUT_ENABLE1_OE_PIN, HIGH);
   delay(10);
 
 }
 
 //////////////////////////////////////////////////////////
 void loop() {
+  //Read Variables/////////
+  uint16_t data_id;
+  size_t command_data_size;
+  uint8_t data_value;
+
   delay(100);
   RoveComm.read(&data_id, &command_data_size, data_value);
 
-  if(data_id = CAMERA_MUX_CHANNEL)
+  if(data_id == CAMERA_MUX_CHANNEL)
   {
     switch(data_value[0])
     {
       case(MUX_CAMERA_1):
-        digitalWrite(MUX1_PIN, LOW);
-        digitalWrite(MUX2_PIN, LOW);
+        digitalWrite(MUX1_A1_PIN, LOW);
+        digitalWrite(MUX1_A2_PIN, LOW);
         break;
       case(MUX_CAMERA_2):
-        digitalWrite(MUX1_PIN, HIGH);
-        digitalWrite(MUX2_PIN, LOW);
+        digitalWrite(MUX1_A1_PIN, HIGH);
+        digitalWrite(MUX1_A2_PIN, LOW);
         break;
       case(MUX_CAMERA_3):
-        digitalWrite(MUX1_PIN, LOW);
-        digitalWrite(MUX2_PIN, HIGH);
+        digitalWrite(MUX1_A1_PIN, LOW);
+        digitalWrite(MUX1_A2_PIN, HIGH);
         break;
       case(MUX_CAMERA_4):
-        digitalWrite(MUX1_PIN, HIGH);
-        digitalWrite(MUX2_PIN, HIGH);
+        digitalWrite(MUX1_A1_PIN, HIGH);
+        digitalWrite(MUX1_A2_PIN, HIGH);
         break;
+      default:
+	    break;
     }
   }
 }
